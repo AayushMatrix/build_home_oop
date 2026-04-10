4cdf4041088524fb2a9ce662b8320cdec2713a43
AayushMatrix <aayushgyawali@gmail.com>
2026-04-10 23:22:44 +0545

    static method was used to track labour attendance when they have logged in or out

--- src/main/labours/labour_class.py
+++ src/main/labours/labour_class.py
@@ -1,4 +1,5 @@
 from loguru import logger
+from datetime import datetime
 
 class Labour:
     def __init__(self,first_name,last_name,wage,role,crud):
@@ -7,8 +8,7 @@ class Labour:
         self.wage = wage 
         self.role = role
         self.crud = crud
-        self.__save_to_databse(self.crud)
-      
+        self.__save_to_databse(self.crud)    
        
 
     def __save_to_databse(self,crud):
@@ -35,15 +35,66 @@ class Labour:
         return insert_result[0][0]
         # db_connection.save()
 
-    def login(self):
-        pass 
+    @staticmethod
+    def login_and_logout(crud, id=None, first_name=None, last_name=None):
+        try:
+            if id is None:
+                if first_name is None or last_name is None:
+                    logger.error("Please provide either id or first name and last name")
+                    return
+                
+                query = """
+                        SELECT id FROM labours
+                        WHERE lower(first_name) = %s
+                        AND lower(last_name) = %s
+                        """
+                result = crud.read_from_pos(query, (first_name.lower(), last_name.lower()))
+                
+                if not result:
+                    logger.error("Labour is not present. Please register first")
+                    return
+                
+                id = result[0][0]
+                logger.info(f"Id from labours table {id}")
+
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            current_date = datetime.now().strftime('%Y-%m-%d')
+
+            check_query = """
+                            SELECT id, start_time, end_time FROM attendance
+                            WHERE labour_id = %s AND DATE(start_time) = %s
+                        """
+            
+            result = crud.read_from_pos(check_query, (id, current_date))
+            logger.info(f"Data from attendance table {result}")
+
+            if len(result)==0:
+                insert_query = f""" 
+                    INSERT INTO attendance (labour_id,start_time)
+                    VALUES (%s,%s)
+                """
+                crud.insert_from_pos(insert_query,(id,current_time))
+                logger.info(f"Labour {id} logged in at {current_time}")
+
+            else:
+                id = result[0][0]
+                update_query = f"""
+                                UPDATE attendance 
+                                SET end_time = %s
+                                WHERE id = %s
+                                 """
+                crud.insert_from_pos(update_query,(current_time,id))
+                logger.info(f"Labour {id} logged out at {current_time}")
+
+        except Exception as e:
+            logger.error(f"Error in login_and_logout: {e}")
 
 
 
 # config = configparser.ConfigParser()
 # config.read(r"C:\Users\Aayush-Gyawali\Desktop\python\src\resources\config_file.ini")
 
-# postgre_db_conn_obj = PostgresConnection(config)
+# postgre_d b_conn_obj = PostgresConnection(config)
 # postgre_db_conn_obj.connect()
 
 # crud = PostgresCRUDOperation(postgre_db_conn_obj.connection)
