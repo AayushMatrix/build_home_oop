8fef19b7784217039adf7a41c5b38ccabd2335b1
AayushMatrix <aayushgyawali@gmail.com>
2026-04-12 00:03:58 +0545

    Created Mistri Object

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,5 @@
 from src.main.databases.postgre_connector import logger,PostgresConnection,PostgresCRUDOperation
-from src.main.labours.labour_class import Labour
+from src.main.labours.labour_class import Labour,Person, Mistri
 
 
 import configparser
@@ -8,29 +8,13 @@ config.read(r"C:\Users\Aayush-Gyawali\Desktop\python\src\resources\config_file.i
 
 
 def main():
-    # pos_db_connection = PostgresConnection(config)
-    # pos_db_connection.connect() 
-
-    # crud_operation_obj = PostgresCRUDOperation(pos_db_connection.connection)
-    # final_result = crud_operation_obj.read_from_pos("select name from labours_table")
-
-    # logger.info(f"{final_result}")
-    
-    # pos_db_connection.close()
-
-    # query = "select * from labours_table"                    
-    # final_result = read_from_postgres(config,query)
-    # logger.info(f"{final_result}")
 
     postgre_db_conn_obj = PostgresConnection(config)
     postgre_db_conn_obj.connect()
 
     crud = PostgresCRUDOperation(postgre_db_conn_obj.connection)
 
-    mansih_obj = Labour("Chabi","Bohara",100,"Mistri",crud)
-    # mansih_obj.save_to_databse(crud)
-
-    # ram_obj = Labour("Ram","Sing",400,"Mistri")
+    mistri_obj = Mistri("Ram","Lal",1000,"mistri",["Plumbing","painter"],crud)
     
 if __name__== "__main__":
     main()
